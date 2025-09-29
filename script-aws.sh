ae3d1b0ef68cc2bfc41f267f596959c276d2bfad
Vitoria <vitoriasilvaamelia@gmail.com>
2025-09-28 21:40:06 -0300

    Update script-aws.sh

--- script-aws.sh
+++ script-aws.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Configurações iniciais
-MY_DB_NAME="dbprojetowp"
-MY_DB_USER="admin"
-MY_DB_PASS="teste_banco"
-MY_EFS_ID="fs-0779fa6e745be3737"
-MY_DB_ENDPOINT="dbprojetowp.cm3muik4uvpm.us-east-1.rds.amazonaws.com"
-MY_DOCKER_COMPOSE_VER="v2.34.0"
-MY_PROJECT_PATH="/home/ec2-user/projeto-docker"
-MY_EFS_PATH="/mnt/efs"
+db_name="dbprojetowp"
+db_user="admin"
+db_pass="teste_banco"
+efs_id="fs-0779fa6e745be3737"
+db_endpoint="dbprojetowp.cm3muik4uvpm.us-east-1.rds.amazonaws.com"
+docker_compose_ver="v2.34.0"
+project_path="/home/ec2-user/projeto-docker"
+efs_path="/mnt/efs"
 
 # Atualizações básicas
 yum update -y
@@ -21,16 +21,16 @@ systemctl start docker
 usermod -a -G docker ec2-user
 
 # Instalação do Docker Compose
-curl -SL https://github.com/docker/compose/releases/download/${MY_DOCKER_COMPOSE_VER}/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/${docker_compose_ver}/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Instalação do EFS utils
 yum install -y amazon-efs-utils
-mkdir -p ${MY_EFS_PATH}
+mkdir -p ${efs_path}
 
 # Retry para montar o EFS
 retries=5
-until mount -t efs ${MY_EFS_ID}:/ ${MY_EFS_PATH}; do
+until mount -t efs ${efs_id}:/ ${efs_path}; do
   ((retries--))
   if [ $retries -le 0 ]; then
     echo "Falha ao montar o EFS após várias tentativas" >> /var/log/user-data.log
@@ -41,14 +41,14 @@ until mount -t efs ${MY_EFS_ID}:/ ${MY_EFS_PATH}; do
 done
 
 # EFS montado automaticamente toda vez que a instância iniciar
-echo "${MY_EFS_ID}:/ ${MY_EFS_PATH} efs defaults,_netdev 0 0" >> /etc/fstab
+echo "${efs_id}:/ ${efs_path} efs defaults,_netdev 0 0" >> /etc/fstab
 
 # Permissões
-chmod -R 777 ${MY_EFS_PATH}
+chmod -R 777 ${efs_path}
 
 # Criação e acesso ao diretório do projeto
-mkdir -p ${MY_PROJECT_PATH}
-cd ${MY_PROJECT_PATH}
+mkdir -p ${project_path}
+cd ${project_path}
 
 # Criação do docker-compose.yml
 cat > docker-compose.yml <<EOL
@@ -58,18 +58,18 @@ services:
     image: wordpress:latest
     container_name: wordpress
     environment:
-      WORDPRESS_DB_HOST: ${MY_DB_ENDPOINT}
-      WORDPRESS_DB_NAME: ${MY_DB_NAME}
-      WORDPRESS_DB_USER: ${MY_DB_USER}
-      WORDPRESS_DB_PASSWORD: ${MY_DB_PASS}
+      WORDPRESS_DB_HOST: ${db_endpoint}
+      WORDPRESS_DB_NAME: ${db_name}
+      WORDPRESS_DB_USER: ${db_user}
+      WORDPRESS_DB_PASSWORD: ${db_pass}
     ports:
       - 80:80
     volumes:
-      - ${MY_EFS_PATH}:/var/www/html
+      - ${efs_path}:/var/www/html
 EOL
 
 # Só iniciar se o EFS estiver montado
-if mountpoint -q ${MY_EFS_PATH}; then
+if mountpoint -q ${efs_path}; then
   docker-compose up -d
   echo "WordPress iniciado com sucesso" >> /var/log/user-data.log
 else
